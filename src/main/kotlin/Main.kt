cbcda9c5ed7a5d97a7715fb6c01b924522caa2ae
Sven Sprey <juette.sven@gmail.com>
2026-03-16 17:17:55 +0100

    Add AST generation and printing functionality
    - Introduced `Expr` sealed class hierarchy for abstract syntax tree (AST) representation.
    - Implemented `GenerateAst` for dynamic AST class generation.
    - Added `AstPrinter` for printing human-readable representations of AST expressions.
    - Updated `Main` to integrate with `AstPrinter`.
    - Made `Token` class public to support AST construction.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,8 @@
 package at.kblizz
 
+import at.kblizz.ast.AstPrinter
+
 fun main(args: Array<String>) {
+    AstPrinter().main()
     KBlizz().start(args)
 }
--- src/main/kotlin/token/Token.kt
+++ src/main/kotlin/token/Token.kt
@@ -1,6 +1,6 @@
 package at.kblizz.token
 
-internal class Token(val type: TokenType?, val lexeme: String?, val literal: Any?, val line: Int) {
+class Token(val type: TokenType?, val lexeme: String?, val literal: Any?, val line: Int) {
     override fun toString(): String {
         return "$type $lexeme $literal"
     }
